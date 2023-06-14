d8b8ccc4316015d458e8b687fa94c4e7844eb63f
hansputera <hanifdwyputrasembiring@gmail.com>
2023-06-14 06:52:48 +0000

    feat: return the results instead than collecting the data
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- src/parsers/users.ts
+++ src/parsers/users.ts
@@ -4,17 +4,14 @@ import {toDom} from '@/util';
 export const getUsersParser = (script: string): User[] => {
 	const $ = toDom(script);
 
-	const users = new Array<User>();
-	$('#datatabel1 thead').next().find('tr').each((_, element) => {
+	return $('#datatabel1 thead').next().find('tr').map((_, element) => {
 		const $u = $(element).find('td');
 
-		users.push({
+		return {
 			nama: $u.eq(1).text().trim(),
 			user: $u.eq(2).text().trim(),
 			level: $u.eq(3).text().trim() as UserRole,
 			status: $u.eq(-1).text().trim().toLowerCase(),
-		});
-	});
-
-	return users;
+		};
+	}).toArray<User>();
 };
